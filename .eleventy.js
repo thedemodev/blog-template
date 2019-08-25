410bd06bb81a8331917e1f5cf16141de8e5252ea
Ben Haynes <ben@rngr.org>
2019-08-24 23:55:17 -0400

    helpers

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,21 @@ module.exports = eleventyConfig => {
   eleventyConfig.addPassthroughCopy('src/images');
   eleventyConfig.addPassthroughCopy('src/fonts');
 
+  // Helpers: 11ty Universal Shortcode
+  eleventyConfig.addShortcode("toLowerCase", function(str) {
+    return str.toLowerCase();
+  });
+
+  eleventyConfig.addShortcode("formatDate", function(datetime) {
+    var datetime  = new Date(datetime);
+    var options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return datetime.toLocaleDateString("en-US", options);
+  });
+
+  eleventyConfig.addShortcode("json", function(context) {
+    return JSON.stringify(context, null, 4);
+  });
+
   return {
     dir: { input: 'src', output: 'dist', data: '_data' },
     passthroughFileCopy: true,
